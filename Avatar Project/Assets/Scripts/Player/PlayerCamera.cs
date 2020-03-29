0979b2a6f61e5d5e14443430c6b04e2d6d561ce9
Mfknudsen <35879356+Mfknudsen@users.noreply.github.com>
2020-03-29 21:40:10 +0200

    +

---
+++ Avatar Project/Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    #region Public Data
+    public GameObject Cam;
+    public Transform Target, Player;
+    #endregion
+
+    #region Private Data
+    private float rotSpeed = 1;
+    private float mouseX, mouseY;
+    #endregion
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        GetInput();
+
+        Rotation();
+    }
+
+    private void GetInput()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+    }
+
+    private void Rotation()
+    {
+        transform.LookAt(Target);
+
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+    }
+}
---
+++ Avatar Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Public Data
+    public GameObject PlayerModel;
+    public Transform CamTransform;
+    public Transform CamHolder;
+    public Transform TargetPos;
+    public Transform temp;
+    #endregion
+
+    #region Private Data
+    private PlayerCamera PlayerCam;
+    private Vector3 MoveDir, RotDir;
+    private float inputX, inputZ;
+    [SerializeField]
+    private float moveSpeed = 1;
+    [SerializeField]
+    private float rotSpeed = 1;
+    private bool isGrounded = false;
+    private bool jumpNow = false;
+    #endregion
+
+    private void Start()
+    {
+        PlayerCam = GetComponent<PlayerCamera>();
+    }
+
+    private void Update()
+    {
+        GetInput();
+
+        Movement();
+        Rotation();
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        GroundDetect();
+    }
+
+    private void GetInput()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.Space) && !jumpNow && isGrounded)
+            jumpNow = true;
+
+    }
+
+    private void Movement()
+    {
+        if (inputZ != 0)
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position + PlayerModel.transform.forward * inputZ * moveSpeed * Time.deltaTime, 0.5f);
+        }
+    }
+
+    private void Rotation()
+    {
+        PlayerModel.transform.rotation = Quaternion.Euler(new Vector3(0,PlayerModel.transform.rotation.eulerAngles.y + inputX * rotSpeed * Time.deltaTime,0));
+    }
+
+    private void Jump()
+    {
+        if (jumpNow)
+        {
+
+            jumpNow = false;
+        }
+    }
+
+    private void GroundDetect()
+    {
+
+    }
+}
